8c47b79272b3be40eab3c2f49d733bb430b9655c
Stan Roelofs <stan@stanroelofs.nl>
2023-02-13 19:34:33 +0100

    Added interfaces for serializing components

---
+++ include/gandalf/serializable.h
@@ -0,0 +1,19 @@
+#ifndef __GANDALF_SNAPSHOT_H
+#define __GANDALF_SNAPSHOT_H
+
+#include <iostream>
+
+namespace gandalf
+{
+    class Serializable
+    {
+    public:
+        /// Serializes the snapshot to the given stream
+        virtual void Serialize(std::ostream& os) const = 0;
+
+        /// Deserializes the snapshot from the given stream
+        virtual void Deserialize(std::istream& is) = 0;
+    };
+}
+
+#endif
