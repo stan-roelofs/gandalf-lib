423ed060e5903142dde7ebba7840a214676e8a5f
Stan Roelofs <stan@stanroelofs.nl>
2023-02-13 20:00:28 +0100

    Added functions to serialize arrays/vectors

--- include/gandalf/serialization.h
+++ include/gandalf/serialization.h
@@ -1,8 +1,11 @@
 #ifndef __GANDALF_SERIALIZATION_H
 #define __GANDALF_SERIALIZATION_H
 
+#include <array>
 #include <cstdint>
 #include <iostream>
+#include <type_traits>
+#include <vector>
 
 namespace gandalf
 {
@@ -18,6 +21,21 @@ namespace gandalf
             }
         }
 
+        template <typename T, std::size_t N>
+        inline void Serialize(std::ostream& os, const std::array<T, N>& values)
+        {
+            for (const auto& value : values)
+                Serialize(os, value);
+        }
+
+        template <typename T>
+        inline void Serialize(std::ostream& os, const std::vector<T>& values)
+        {
+            Serialize(os, values.size());
+            for (const auto& value : values)
+                Serialize(os, value);
+        }
+
         template <typename T>
         inline void Deserialize(std::istream& is, T& value)
         {
@@ -26,6 +44,23 @@ namespace gandalf
             for (size_t byte = 0; byte < sizeof(T); ++byte) {
                 value |= static_cast<T>(is.get()) << (byte * 8);
             }
+        }
+
+        template <typename T, std::size_t N>
+        inline void Deserialize(std::istream& is, std::array<T, N>& values)
+        {
+            for (auto& value : values)
+                Deserialize(is, value);
+        }
+
+        template <typename T>
+        inline void Deserialize(std::istream& is, std::vector<T>& values)
+        {
+            std::size_t size;
+            Deserialize(is, size);
+            values.resize(size);
+            for (auto& value : values)
+                Deserialize(is, value);
         }
     }
 }
--- tests/src/serialization_test.cpp
+++ tests/src/serialization_test.cpp
@@ -1,7 +1,9 @@
 #include <gandalf/serialization.h>
 
+#include <array>
 #include <limits>
 #include <gtest/gtest.h>
+#include <vector>
 
 using namespace gandalf;
 using namespace serialization;
@@ -26,126 +28,146 @@ TEST(Serialization, serialize_uint8_max)
 
 TEST(Serialization, serialize_uint16_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint16_t>::min());
-	std::uint16_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint16_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint16_t>::min());
+    std::uint16_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint16_t>::min());
 }
 
 TEST(Serialization, serialize_uint16_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint16_t>::max());
-	std::uint16_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint16_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint16_t>::max());
+    std::uint16_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint16_t>::max());
 }
 
 TEST(Serialization, serialize_uint32_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint32_t>::min());
-	std::uint32_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint32_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint32_t>::min());
+    std::uint32_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint32_t>::min());
 }
 
 TEST(Serialization, serialize_uint32_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint32_t>::max());
-	std::uint32_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint32_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint32_t>::max());
+    std::uint32_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint32_t>::max());
 }
 
 TEST(Serialization, serialize_uint64_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint64_t>::min());
-	std::uint64_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint64_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint64_t>::min());
+    std::uint64_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint64_t>::min());
 }
 
 TEST(Serialization, serialize_uint64_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::uint64_t>::max());
-	std::uint64_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::uint64_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::uint64_t>::max());
+    std::uint64_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::uint64_t>::max());
 }
 
 TEST(Serialization, serialize_int8_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int8_t>::min());
-	std::int8_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int8_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int8_t>::min());
+    std::int8_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int8_t>::min());
 }
 
 TEST(Serialization, serialize_int8_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int8_t>::max());
-	std::int8_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int8_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int8_t>::max());
+    std::int8_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int8_t>::max());
 }
 
 TEST(Serialization, serialize_int16_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int16_t>::min());
-	std::int16_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int16_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int16_t>::min());
+    std::int16_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int16_t>::min());
 }
 
 TEST(Serialization, serialize_int16_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int16_t>::max());
-	std::int16_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int16_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int16_t>::max());
+    std::int16_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int16_t>::max());
 }
 
 TEST(Serialization, serialize_int32_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int32_t>::min());
-	std::int32_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int32_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int32_t>::min());
+    std::int32_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int32_t>::min());
 }
 
 TEST(Serialization, serialize_int32_max)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int32_t>::max());
-	std::int32_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int32_t>::max());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int32_t>::max());
+    std::int32_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int32_t>::max());
 }
 
 TEST(Serialization, serialize_int64_min)
 {
-	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int64_t>::min());
-	std::int64_t value;
-	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int64_t>::min());
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int64_t>::min());
+    std::int64_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int64_t>::min());
 }
 
 TEST(Serialization, serialize_int64_max)
 {
+    std::stringstream ss;
+    Serialize(ss, std::numeric_limits<std::int64_t>::max());
+    std::int64_t value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, std::numeric_limits<std::int64_t>::max());
+}
+
+TEST(Serialization, serialize_array)
+{
+    std::stringstream ss;
+    std::array<std::uint8_t, 3> array = { 1, 2, 3 };
+    Serialize(ss, array);
+    std::array<std::uint8_t, 3> value;
+    Deserialize(ss, value);
+    EXPECT_EQ(value, array);
+}
+
+TEST(Serialization, serialize_vector)
+{
 	std::stringstream ss;
-	Serialize(ss, std::numeric_limits<std::int64_t>::max());
-	std::int64_t value;
+	std::vector<std::uint8_t> vector = { 1, 2, 3 };
+	Serialize(ss, vector);
+	std::vector<std::uint8_t> value;
 	Deserialize(ss, value);
-	EXPECT_EQ(value, std::numeric_limits<std::int64_t>::max());
+	EXPECT_EQ(value, vector);
 }
